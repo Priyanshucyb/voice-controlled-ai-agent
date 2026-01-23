38cc99e7e79a4e040b6a456ef11e8be610369bef
Priyanshucyb <priyanshu_ua2503aih300@iitp.ac.in>
2026-01-23 22:15:41 +0530

    Update voice_input.py

--- voice_input.py
+++ voice_input.py
@@ -1,4 +1,25 @@
+import speech_recognition as sr
+
+
 def listen():
-    text = input("Type command: ")
-    print("You said:", text)
-    return text
+    r = sr.Recognizer()
+
+    try:
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source, duration=0.5)
+            audio = r.listen(source, timeout=5)
+
+        text = r.recognize_google(audio)
+        return text.lower()
+
+    except sr.WaitTimeoutError:
+        print("Timeout: kuch bola nahi")
+        return ""
+
+    except sr.UnknownValueError:
+        print("Samajh nahi aaya, dubara bolo")
+        return ""
+
+    except sr.RequestError as e:
+        print(f"Speech service error: {e}")
+        return
